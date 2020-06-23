d992d4b02883c90336d2c7e793c7040c0f854954
Edd Salkield <edd@salkield.ukgit config --global user.email edd@salkield.ukgit config --global user.email edd@salkield.uk>
2020-06-23 13:15:19 +0100

    jbidwatcher_importer code cleanup

--- jbidwatcher_importer/jbidwatcher_importer.py
+++ jbidwatcher_importer/jbidwatcher_importer.py
@@ -6,8 +6,7 @@ from typing import List
 from collections import OrderedDict
 from termcolor import colored
 import traceback
-
-import sys
+from numbers import Number
 
 def _db_transaction_factory(db_path):
     def _db_transaction(f):
@@ -40,10 +39,44 @@ def _db_transaction_factory(db_path):
     return _db_transaction
 
 
-ebay_listings_entries = ['listing_id', 'title', 'seller', 'start_time', \
-        'end_time', 'n_bids', 'price', 'price_currency', 'buy_now_price', \
-        'buy_now_price_currency', 'starting_price', 'starting_price_currency', \
-        'winner', 'location_id', 'image_paths']
+# merges d2 into d1
+def _merge_dicts(d1: dict, d2: dict):
+    for k,v2 in d2.items():
+        try:
+            v1 = d1[k]
+        except KeyError:
+            d1[k] = v2
+            continue
+
+        if v1 is None:
+            d1[k] = v2
+            continue
+        elif v2 is None:
+            d1[k] = v1
+            continue
+
+        if isinstance(v1, Number) and isinstance(v2, Number):
+            if v2 > v1:
+                d1[k] = v2
+            continue
+
+        if type(v1) != type(v2):
+            raise ValueError('{} and {} not of same types: {}, {}'.format(\
+                    v1, v2, type(v1), type(v2)))
+
+        if isinstance(v1, str):
+            d1[k] = v2
+            continue
+
+        raise ValueError('Unknown failure between {}:{} and {}:{}'\
+                .format(v1, type(v1), v2, type(v2)))
+    return d1
+
+
+
+ebay_listings_entries = ['auction_id', 'title', 'seller', 'start_time', \
+        'end_time', 'n_bids', 'price', 'currency_code', 'buy_now_price', \
+        'starting_price', 'winner', 'location_id', 'image_paths', 'description']
 
 def main(db_path: str, paths: List[str]):
     for path in paths:
@@ -61,32 +94,32 @@ def jbw_import(db_path, path):
     @_db_transaction_factory(db_path)
     def _(c):
         c.execute('''
-            CREATE TABLE IF NOT EXISTS ebay_listings (
-                listing_id INTEGER NOT NULL PRIMARY KEY,
+            CREATE TABLE IF NOT EXISTS ebay_auctions (
+                auction_id INTEGER NOT NULL PRIMARY KEY,
                 title TEXT,
                 seller TEXT,   -- Primary key of sellers table
                 start_time INTEGER,
                 end_time INTEGER,
                 n_bids INTEGER,
                 price INTEGER,
-                price_currency TEXT,
+                currency_code TEXT,
                 buy_now_price INTEGER,
-                buy_now_price_currency TEXT,
                 starting_price INTEGER,
-                starting_price_currency TEXT,
                 winner TEXT,
                 location_id TEXT,  -- Primary key of locations table
-                image_paths BLOB NOT NULL -- paths relative to some base path,
+                image_paths TEXT NOT NULL, -- colon-separated paths relative to some base path
+                description TEXT
             )
         ''')
 
         c.execute('''
-        CREATE TABLE IF NOT EXISTS ebay_sellers (
-                seller_id TEXT NOT NULL PRIMARY KEY,
+        CREATE TABLE IF NOT EXISTS ebay_profiles (
+                profile_id TEXT NOT NULL PRIMARY KEY,
                 description TEXT,
                 contacted INTEGER NOT NULL,
                 email TEXT,
                 location TEXT,
+                name TEXT,
                 registered INTEGER,
                 permission_given INTEGER NOT NULL,
                 member_since TEXT,
@@ -94,20 +127,9 @@ def jbw_import(db_path, path):
                 n_followers INTEGER,
                 n_reviews INTEGER,
                 percent_positive_feedback INTEGER
+
             )
         ''')
-
-        c.execute('''
-            CREATE TABLE IF NOT EXISTS ebay_locations (
-                location_id TEXT NOT NULL PRIMARY KEY,
-                place_name TEXT NOT NULL,
-                country_name TEXT NOT NULL,
-                latitude REAL NOT NULL,
-                longitude REAL NOT NULL,
-                notes TEXT
-            )
-        ''')
-
 
     with open(path, errors='ignore') as fd:
         print(path)
@@ -124,17 +146,19 @@ def jbw_import(db_path, path):
 
 def process_auction(a, db_path):
     auction = a['info']
-    listing_id = a['@id']
+    auction_id = a['@id']
 
+    existing_auction_dict = {}
     auction_dict = {}
     for e in ebay_listings_entries:
+        existing_auction_dict[e] = None
         auction_dict[e] = None
 
     # Determine if an auction of the given id currently exists
     @_db_transaction_factory(db_path)
     def existing_entry(c):
-        c.execute('SELECT * FROM ebay_listings WHERE listing_id=?',\
-                (listing_id,))
+        c.execute('SELECT * FROM ebay_auctions WHERE auction_id=?',\
+                (auction_id,))
         r = c.fetchall()
         if not r:
             return None
@@ -145,187 +169,114 @@ def process_auction(a, db_path):
         # Merge existing entries
         for name, e in zip(ebay_listings_entries, existing_entry):
             if e is not None:
-                auction_dict[name] = e
-
+                existing_auction_dict[name] = e
 
     try:
-        title = auction['title']
+        auction_dict['title'] = auction['title']
     except KeyError as e:
-        title = None    # Some strange jbidwatcher entries don't have titles
+        pass
     try:
         seller = auction['seller']
     except KeyError:
-        seller = None
+        pass
     if isinstance(seller, str):
-        seller_name = seller
+        auction_dict['seller'] = seller
     elif isinstance(seller, OrderedDict):
-        seller_name = seller['name']
-    elif seller is None:
-        seller_name = None
+        auction_dict['seller'] = seller['name']
+    elif auction_dict['seller'] is None:
+        seller = None
     else:
         raise ValueError('Auction: {}\nSeller {} of invalid type: {}'\
-                .format(listing_id, str(seller), str(type(seller))))
+                .format(auction_id, str(seller), str(type(seller))))
 
 
     # jbidwatcher timestamps are UNIX time in ms
     try:
-        start_time = int(auction['start'])/1000 \
+        auction_dict['start_time'] = int(auction['start'])/1000 \
                 if int(auction['start']) > 0 else None
     except KeyError:
-        start_time = None
+        pass
 
     try:
-        end_time = int(auction['end'])/1000 if int(auction['end']) > 0 else None
+        auction_dict['end_time'] = \
+                int(auction['end'])/1000 if int(auction['end']) > 0 else None
     except KeyError:
-        end_time = None
+        pass
 
     try:
-        n_bids = int(auction['bidcount']) if int(auction['bidcount']) > 0 else 0
+        auction_dict['n_bids'] = \
+                int(auction['bidcount']) if int(auction['bidcount']) > 0 else 0
     except KeyError:
-        n_bids = None
+        pass
     try:
-        winner = auction['highbidder']
+        auction_dict['winner'] = auction['highbidder']
     except KeyError:
-        winner = None
+        pass
 
+    currency_code = None
     try:
         if auction['currently']['@currency'] != 'UNK':
-            price = float(auction['currently']['@price'])
-            price_currency = auction['currently']['@currency']
-        else:
-            price = None
-            price_currency = None
+            auction_dict['price'] = float(auction['currently']['@price'])
+            auction_dict['currency_code'] = auction['currently']['@currency']
     except KeyError:
-        price = None
-        price_currency = None
+        pass
 
     try:
         if auction['buynow']['@currency'] != 'UNK':
-            buy_now_price = float(auction['buynow']['@price'])
-            buy_now_price_currency = auction['buynow']['@currency']
-        else:
-            buy_now_price = None
-            buy_now_price_currency = None
+            auction_dict['buy_now_price'] = float(auction['buynow']['@price'])
+            auction_dict['currency_code'] = auction['buynow']['@currency']
     except KeyError:
-        buy_now_price = None
-        buy_now_price_currency = None
+        pass
 
     try:
         if auction['minimum']['@currency'] != 'UNK':
-            starting_price = float(auction['minimum']['@price'])
-            starting_price_currency = auction['minimum']['@currency']
-        else:
-            starting_price = None
-            starting_price_currency = None
+            auction_dict['starting_price'] = float(auction['minimum']['@price'])
+            auction_dict['currency_code'] = auction['minimum']['@currency']
     except KeyError:
-        starting_price = None
-        starting_price_currency = None
+        pass
 
     # Merge the auctions into one
     current_newer = False
     if existing_entry is not None:
         # Determine which entry is newer
         try:
-            current_newer = n_bids > auction_dict['n_bids'] \
-                    or end_time > auction_dict['end_time'] \
-                    or (price_currency == auction_dict['price_currency'] \
-                    and price > auction_dict['price'])
+            current_newer = auction_dict['n_bids'] > existing_auction_dict['n_bids'] \
+                    or auction_dict['end_time'] > existing_auction_dict['end_time'] \
+                    or auction_dict['price'] > existing_auction_dict['price']
         except TypeError:
             # Short-circuiting of previous expressions will fail if evaluates
             # in place of None
             pass
-    
-        #if current_newer:
-        #    print("Collision found.  Resolving by updating existing older record with newer information")
-        #else:
-        #    print("Collision found.  Resolving by updating exising newer information with missing gaps")
-        #print(auction)
 
-    # Merge the auctions
     if current_newer:
-        if title is not None:
-            auction_dict['title'] = title
-        if seller_name is not None:
-            auction_dict['seller'] = seller_name
-        if start_time is not None:
-            auction_dict['start_time'] = start_time 
-        if end_time is not None:
-            auction_dict['end_time'] = end_time
-        if winner is not None:
-            auction_dict['winner'] = winner
-    elif existing_entry is None:
-        auction_dict['title'] = title
-        auction_dict['seller'] = seller_name
-        auction_dict['start_time'] = start_time
-        auction_dict['end_time'] = end_time
-        auction_dict['winner'] = winner
-                
-    if existing_entry is not None:
-        if auction_dict['n_bids'] is None or \
-                (n_bids is not None and n_bids > auction_dict['n_bids']):
-            auction_dict['n_bids'] = n_bids
-
-        if auction_dict['price_currency'] is None \
-                or (price_currency is not None \
-                and auction_dict['price'] is not None \
-                and price_currency == auction_dict['price_currency'] \
-                and price > auction_dict['price']):
-            auction_dict['price'] = price
-            auction_dict['price_currency'] = price_currency
-            
-        if auction_dict['buy_now_price_currency'] is None \
-                or (buy_now_price_currency is not None \
-                and auction_dict['buy_now_price'] is not None \
-                and buy_now_price_currency == auction_dict['buy_now_price_currency'] \
-                and buy_now_price > auction_dict['buy_now_price']):
-            auction_dict['buy_now_price'] = buy_now_price
-            auction_dict['buy_now_price_currency'] = buy_now_price_currency
-            
-        if auction_dict['starting_price_currency'] is None \
-                or (starting_price_currency is not None \
-                and auction_dict['starting_price'] is not None \
-                and starting_price_currency == auction_dict['starting_price_currency'] \
-                and starting_price > auction_dict['starting_price']):
-            auction_dict['starting_price'] = starting_price
-            auction_dict['starting_price_currency'] = starting_price_currency
-            
-            
+        a = _merge_dicts(existing_auction_dict, auction_dict)
     else:
-        auction_dict['n_bids'] = n_bids
-        auction_dict['price'] = price
-        auction_dict['price_currency'] = price_currency
-        auction_dict['buy_now_price'] = buy_now_price
-        auction_dict['buy_now_price'] = buy_now_price_currency
-        auction_dict['starting_price_currency'] = starting_price
-        auction_dict['starting_price_currency'] = starting_price_currency
+        a = _merge_dicts(auction_dict, existing_auction_dict)
 
     # Write out to the database
     @_db_transaction_factory(db_path)
     def _(c):
         # Delete the row
         if existing_entry is not None:
-            c.execute('DELETE FROM ebay_listings WHERE listing_id=?', \
-                    (listing_id,))
+            c.execute('DELETE FROM ebay_auctions WHERE auction_id=?', \
+                    (auction_id,))
 
         # Update the row to its new values
-        c.execute('''INSERT INTO ebay_listings (
-            listing_id, title, seller, start_time, end_time, n_bids,
-            price, price_currency, starting_price,
-            starting_price_currency, image_paths) VALUES (
-            ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ""
+        c.execute('''INSERT INTO ebay_auctions (
+            auction_id, title, seller, start_time, end_time, n_bids,
+            price, currency_code, starting_price, image_paths) VALUES (
+            ?, ?, ?, ?, ?, ?, ?, ?, ?, ""
             )''',
-            (listing_id, auction_dict['title'], auction_dict['seller'], \
-                auction_dict['start_time'], auction_dict['end_time'], \
-                auction_dict['n_bids'], auction_dict['price'], \
-                auction_dict['price_currency'], auction_dict['starting_price'], \
-                auction_dict['starting_price_currency']))
+            (auction_id, a['title'], a['seller'], a['start_time'], \
+                a['end_time'], a['n_bids'], a['price'], a['currency_code'], \
+                a['starting_price']))
 
     # Update the sellers table
     @_db_transaction_factory(db_path)
     def _(c):
-        c.execute('''INSERT OR IGNORE INTO ebay_sellers (
-                nickname, contacted, permission_given) VALUES(?, ?, ?)''', \
-                (seller_name, 0, 0,))
+        c.execute('''INSERT OR IGNORE INTO ebay_profiles (
+                profile_id, contacted, permission_given) VALUES(?, ?, ?)''', \
+                (a['seller'], 0, 0,))
 
 
 if __name__ == "__main__":
